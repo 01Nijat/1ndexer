d9ca74d9ecb1f05c06fc09db7eb80ee93eeda559
01Nijat <mr.nijat.asgarov@gmail.com>
2022-12-01 00:26:25 +0400

    Indexer Task

---
+++ indexer/NtCompany.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace indexer
+{
+    public class NtCompany
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        private Employee[] _employees;
+        public NtCompany(int size=10)
+        {
+            _employees=new Employee[size];
+        }
+        public Employee this[int index]
+        {
+            get {
+                try
+                {
+                    return _employees[index];
+                }
+                catch (Exception)
+                {
+
+                    return _employees[0];
+                }
+            }
+            set {
+                try
+                {
+                    _employees[index] = value;
+                }
+                catch (Exception)
+                {
+
+                    Console.WriteLine("out of range");
+                }
+            }
+        }
+    }
+}
---
+++ indexer/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+using indexer;
+using System.Reflection;
+
+Console.WriteLine("Indexer");
+Employee employee1 = new Employee();
+employee1.Id = 1;
+employee1.name = "Nijat";
+NtCompany NtCompany = new NtCompany(11);
+NtCompany[1] = employee1;
+Console.WriteLine(NtCompany[1]);
+
+//Assembly
+//Assembly assembly= Assembly.GetExecutingAssembly();
+//foreach (var item in assembly.GetTypes())
+//{
+//    Console.ForegroundColor= ConsoleColor.Red;
+//    Console.WriteLine(item.Name +" : ");
+//    foreach (var i in item.GetMembers())
+//    {
+//        Console.ForegroundColor = ConsoleColor.Green;
+//        Console.WriteLine(i);
+//    }
+//}
